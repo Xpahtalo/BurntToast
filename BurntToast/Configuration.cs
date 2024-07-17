ec82f40fca072da2ecb6a12c4f9e6994c7cd6d5c
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2024-07-17 16:36:08 -0700

    revert default constructor due to Newtonsoft fix #2

--- BurntToast/Configuration.cs
+++ BurntToast/Configuration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Dalamud.Configuration;
+using Newtonsoft.Json;
 
 namespace BurntToast;
 
@@ -21,6 +22,7 @@ public class Configuration : IPluginConfiguration {
 
     internal void AddToastPattern(Regex regex) {
         Patterns.Add(regex);
+
         if (!string.IsNullOrWhiteSpace(regex.ToString())) { Save(); }
     }
 
@@ -30,6 +32,7 @@ public class Configuration : IPluginConfiguration {
 
     internal void AddBattleTalkPattern(Regex regex, bool showMessage) {
         BattleTalkPatterns.Add(new BattleTalkPattern(regex, showMessage));
+
         if (!string.IsNullOrWhiteSpace(regex.ToString())) { Save(); }
     }
 
@@ -43,10 +46,15 @@ public class Configuration : IPluginConfiguration {
 }
 
 [Serializable]
-public class BattleTalkPattern(Regex pattern, bool showMessage) {
-    public Regex Pattern     { get; set; } = pattern;
-    public bool  ShowMessage { get; set; } = showMessage;
+public class BattleTalkPattern {
+    public Regex Pattern     { get; set; }
+    public bool  ShowMessage { get; set; }
 
-    public BattleTalkPattern(string pattern, bool showMessage) : this(
-        new Regex(pattern, RegexOptions.Compiled), showMessage) { }
+    [JsonConstructor]
+    public BattleTalkPattern(Regex pattern, bool showMessage) {
+        Pattern     = pattern;
+        ShowMessage = showMessage;
+    }
+
+    public BattleTalkPattern(string pattern, bool showMessage) : this(new Regex(pattern, RegexOptions.Compiled), showMessage) { }
 }
